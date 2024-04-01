bb3fe98f5d2f0e086a8f148a9fa45a407ef68e05
Farzan <farzan.dehbashi@achivers.com>
2024-04-01 13:58:26 -0400

    .

---
+++ methods.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type rectangle struct {
+	width, height float64
+}
+
+func (r *rectangle) getArea() float64 {
+	return r.width * r.height
+}
+
+func (r rectangle) getVatar() float64 {
+	return math.Sqrt(r.width*r.width + r.height*r.height)
+}
+
+func main() {
+	r := rectangle{width: 1, height: 1}
+	fmt.Println("area -> ", r.getArea())
+	fmt.Println("vatar ->", r.getVatar())
+
+	rp := &r
+	fmt.Println(rp.getArea())
+	fmt.Println(rp.getVatar())
+}
